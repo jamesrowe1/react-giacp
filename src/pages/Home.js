19275e828d79e9d738b86d23c581da48f08715d5
James Rowe <jamesrowe@outlook.com>
2020-09-18 19:24:19 -0400

    fixed picture

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,8 +4,11 @@ class Home extends Component {
     render() {
       return (
         <>
+        <h4>
+            Our Story
+        </h4>
         <div className="card-bio">
-            Gastroentorology Associates of Central Pennsylvania was founded in 2003 by Dr. William Rowe and Dr. Robert Werkman. They have now expanded to include PA Carrie Leese, and are looking forward to providing whatever Gastroentorologist services you require.
+            Gastroentorology Associates of Central Pennsylvania was founded in 2003 by Dr. William Rowe and Dr. Robert Werkman. They have now expanded to include PA Carrie Leese, and are looking forward to providing whatever Gastroentorological services you require.
         </div>
         <h4>
             Reviews
@@ -14,7 +17,7 @@ class Home extends Component {
             "My Urologist referred me to Dr R for difficulties that have gotten worse following multiple pelvic surgeries. His office had me mail Medical Records in advance, which he had reviewed before we met. His questions were direct, on point, & his recommendations helpful. He answered my questions, making sure I understood the changes I am supposed to make."
         </div>
         <div className="card-bio">
-            "I have been going to Dr Rowe for about 2 years and have had several procedures. Dr. Rowe and his staff are very professional and caring. He was formerly the head of gastroenterology at Hershey Medical Center.Appointments are easy to get. They have the facilities to do upper and lower GIs right on the premise. It is much easier than having to go to the hospital.""
+            "I have been going to Dr Rowe for about 2 years and have had several procedures. Dr. Rowe and his staff are very professional and caring. Appointments are easy to get. They have the facilities to do upper and lower GIs right on the premise. It is much easier than having to go to the hospital.
         </div>
         </>
       );
